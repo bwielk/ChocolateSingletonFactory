6f75a0904feee7241296e7304167932ba858229e
bwielk <bwielk@gmail.com>
2018-01-01 19:20:06 +0100

    Coded ways of implementing the ONLY ONE instantation

--- src/chocolate/ChocolateBoiler.java
+++ src/chocolate/ChocolateBoiler.java
@@ -2,14 +2,34 @@ package chocolate;
 
 public class ChocolateBoiler {
 	
+	//----------------DOUBLE CHECKED LOCKING SOLUTION---------------------
+	//private volatile static ChocolateBoiler uniqueBoiler;
+	private static ChocolateBoiler uniqueBoiler = new ChocolateBoiler();
 	private boolean empty;
 	private boolean boiled;
 	
-	public ChocolateBoiler(){
+	private ChocolateBoiler(){//PRIVATE constructor doesn't allow to instantiate the class externally
 		empty = true;
 		boiled = false;
 	}
 	
+	public static ChocolateBoiler getInstance(){
+		return uniqueBoiler;
+	}
+	
+	/*----------------DOUBLE CHECKED LOCKING SOLUTION---------------------
+	
+	public static ChocolateBoiler getInstance(){   //generates a unique instance of the class
+		if(uniqueBoiler == null){                  //if there is no instance of the class, proceed
+			synchronized(ChocolateBoiler.class){
+				if(uniqueBoiler == null){
+					uniqueBoiler = new ChocolateBoiler();
+				}
+			}
+		}
+		return uniqueBoiler;
+	}
+	*/
 	public void fill(){
 		if(empty){
 			empty = false;
